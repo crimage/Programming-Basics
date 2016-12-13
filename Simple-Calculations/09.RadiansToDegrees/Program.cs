1feb92dd84b00441b11354d9d9b632d364e2792a
crimage <crimag3@gmail.com>
2016-12-13 23:53:31 +0100

    Homework tasks

---
+++ Simple-Calculations/09.RadiansToDegrees/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angle.and.radian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     //     Console.Write("Enter radian angle: ");
+            double rad = double.Parse(Console.ReadLine());
+            double deg = rad * 180 / Math.PI;
+            Console.WriteLine("Radian degree :" + Math.Round(deg));
+        }
+    }
+}
