9a92b861d47e23b8dbfe52d62f2d99cccd8a4fce
crimage <crimag3@gmail.com>
2016-12-14 14:39:19 +0100

    adding advanced loops tasks

---
+++ Advanced-Loops/01.Numbers1...NwithStep3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Numbers1._._.NwithStep3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
