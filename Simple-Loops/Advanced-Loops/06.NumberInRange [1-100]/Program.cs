1d01056907cfc41ba1e924a4949e730cfedc5c5c
crimage <crimag3@gmail.com>
2016-12-14 14:36:00 +0100

    adding advanced loops tasks

---
+++ Simple-Loops/Advanced-Loops/06.NumberInRange [1-100]/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.NumberInRange__1_100_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
